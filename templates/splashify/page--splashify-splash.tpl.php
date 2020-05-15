e1fff2407a4b1ad52ccfaae7e57910950431e252
Joan Galtés i Moreno <donbuche@gmail.com>
2020-05-15 18:18:59 +0200

    Afegits estils CSS per als interstitials fets amb el mòdul Splashify. Afegits dos nous templates per sobreescriure els estils CSS heredats, dins de l'iframe que genera aquest mòdul.

---
+++ templates/splashify/page--splashify-splash.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see template_process()
+ * @see html.tpl.php
+ */
+?>
+
+<?php print render($page['content']); ?>
