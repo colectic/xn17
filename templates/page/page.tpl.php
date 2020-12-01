295c50e0343285699b4820aec7fa83ef9b3ff50f
Joan Galtés i Moreno <donbuche@gmail.com>
2020-12-01 12:36:12 +0100

    Corregit el text alternatiu de la imatge del botó del menú.

--- templates/page/page.tpl.php
+++ templates/page/page.tpl.php
@@ -18,7 +18,7 @@
 	<a href="/"><img src="/sites/all/themes/xn17/assets/images/logo/logo-30-white.svg" alt="logo"/></a>
       </div>
       <div class="menu-icon">
-        <img src="/sites/all/themes/xn17/assets/images/icon/icon-menu-gray.svg" alt="menu icon"/>
+        <img src="/sites/all/themes/xn17/assets/images/icon/icon-menu-gray.svg" alt="Botó per expandir/contraure el menú" role="button" tabindex="0"/>
       </div>
       <div class="main-selector main-resources">
         <a href="/recursos">recursos</a>
